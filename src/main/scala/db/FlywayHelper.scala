6dba504fa1e09f72da83a6d1c29669e7c8a435e2
gerryfletch <1997Gerry@gmail.com>
2020-02-04 18:13:41 +0000

    debug messages

--- src/main/scala/db/FlywayHelper.scala
+++ src/main/scala/db/FlywayHelper.scala
@@ -11,6 +11,8 @@ object FlywayHelper {
     if (flyway.isEmpty) {
       val jdbcUrl: String = "jdbc:" + ConfigFactory.load().getString("ctx.url")
       val password: String = sys.env.getOrElse("DB_PASSWORD", "")
+      println("Connecting with JDBC: " + jdbcUrl)
+      println(s"And password: '${password}'")
       flyway = Option(Flyway.configure()
         .locations("migration")
         .dataSource(jdbcUrl, "root", password)
