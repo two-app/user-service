f0fd70454a837fdcf00458a11c69944f99bd8a5e
gerryfletch <1997Gerry@gmail.com>
2020-02-04 16:50:38 +0000

    Loaded URL from config

--- src/main/scala/db/package.scala
+++ src/main/scala/db/package.scala
@@ -1,3 +1,4 @@
+import com.typesafe.config.ConfigFactory
 import io.getquill._
 import org.flywaydb.core.Flyway
 
@@ -5,9 +6,11 @@ package object db {
 
   lazy val ctx = new MysqlAsyncContext(SnakeCase, "ctx")
 
+  val jdbcUrl: String = "jdbc:" + ConfigFactory.load().getString("ctx.url")
+
   def getFlyway: Flyway = Flyway.configure()
     .locations("migration")
-    .dataSource("jdbc:mysql://localhost:3306/users?user=root", "root", "")
+    .dataSource(jdbcUrl, "root", "")
     .load()
 
 }
