7bbdc99009f8bcc722f402f29d4ef3948b68bd1e
gerryfletch <1997Gerry@gmail.com>
2020-02-05 20:39:06 +0000

    Email validation tests.

--- src/test/scala/user/EmailValidatorTest.scala
+++ src/test/scala/user/EmailValidatorTest.scala
@@ -1,5 +1,14 @@
 package user
 
-class EmailValidatorTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
+class EmailValidatorTest extends AnyFlatSpec with Matchers {
+  "an invalid email" should "fail" in {
+    EmailValidator.isValid("random") shouldBe false
+  }
+
+  "a valid email" should "succeed" in {
+    EmailValidator.isValid("admin@two.com") shouldBe true
+  }
 }
