8e315ee42e7d42ed79565c34ced7d4be358f6e21
gerryfletch <1997Gerry@gmail.com>
2020-02-08 18:24:12 +0000

    User Context functionalised

--- src/main/scala/request/UserContext.scala
+++ src/main/scala/request/UserContext.scala
@@ -1,13 +1,13 @@
 package request
 
 import akka.http.scaladsl.model.HttpRequest
-import pdi.jwt.{Jwt, JwtClaim, JwtOptions}
+import pdi.jwt.{Jwt, JwtOptions}
 import response.ErrorResponse
 import response.ErrorResponse.AuthorizationError
+import spray.json.DefaultJsonProtocol._
+import spray.json._
 
 import scala.util.{Failure, Success}
-import spray.json._
-import DefaultJsonProtocol._
 
 case class UserContext(uid: Int, pid: Option[Int], cid: Option[Int], connectCode: Option[String])
 
@@ -16,34 +16,24 @@ object UserContext {
   implicit val f: RootJsonFormat[UserContext] = jsonFormat4(UserContext.apply)
 
   def from(accessToken: String): Either[ErrorResponse, UserContext] = {
-    val jwt: JwtClaim = Jwt.decode(accessToken, JwtOptions(signature = false, expiration = false, notBefore = false)) match {
-      case Failure(_) => return Left(AuthorizationError("Invalid token format."))
-      case Success(v) => v
+    Jwt.decode(accessToken, JwtOptions(signature = false, expiration = false, notBefore = false)) match {
+      case Failure(_) => Left(AuthorizationError("Invalid token format."))
+      case Success(v) => Right(v.content.parseJson.convertTo[UserContext])
     }
-
-    Right(jwt.content.parseJson.convertTo[UserContext])
   }
 
-  def from(request: HttpRequest): Either[ErrorResponse, UserContext] = {
-    val token: String = extractToken(request) match {
-      case Left(e) => return Left(e)
-      case Right(token) => token
-    }
-
-    this.from(token)
+  def from(request: HttpRequest): Either[ErrorResponse, UserContext] = extractToken(request) match {
+    case Left(e) => Left(e)
+    case Right(token) => this.from(token)
   }
 
   private def extractToken(request: HttpRequest): Either[AuthorizationError, String] = {
-    val header: String = request.headers
-      .find(h => h.lowercaseName() == "authorization")
-      .getOrElse(return Left(AuthorizationError("Authorization not provided.")))
-      .value()
-
-    if (header.isEmpty) return Left(AuthorizationError("Authorization not provided."))
-
-    val parts = header.split(" ")
-    if (parts.length != 2 || parts(0) != "Bearer") return Left(AuthorizationError("Invalid header format."))
-
-    Right(parts(1))
+    request.headers.find(h => h.name().equalsIgnoreCase("Authorization"))
+      .map(header => header.value)
+      .toRight(AuthorizationError("Authorization not provided."))
+      .filterOrElse(t => !t.isBlank, AuthorizationError("Authorization not provided."))
+      .map(header => header.split(" "))
+      .filterOrElse(parts => parts.length == 2 && parts(0) == "Bearer", AuthorizationError("Invalid header format."))
+      .map(parts => parts(1))
   }
 }
