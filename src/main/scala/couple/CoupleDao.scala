bd151f8b0f165f38ea61c604fcb729c911bc643d
gerryfletch <1997Gerry@gmail.com>
2020-02-16 14:03:46 +0000

    Created and tested Couple Dao.

--- src/main/scala/couple/CoupleDao.scala
+++ src/main/scala/couple/CoupleDao.scala
@@ -1,5 +1,37 @@
 package couple
 
-class CoupleDao {
+import com.typesafe.scalalogging.Logger
+import db.ctx._
 
+import scala.concurrent.ExecutionContext.Implicits.{global => ec}
+import scala.concurrent.Future
+
+final case class CoupleRecord(cid: Int, uid: Int, pid: Int)
+
+object CoupleRecord {
+  def asInsertRecord(uid: Int, pid: Int): CoupleRecord = CoupleRecord(0, uid, pid)
+}
+
+trait CoupleDao {
+  def storeCouple(uid: Int, pid: Int): Future[Int]
+
+  def getCouple(cid: Int): Future[Option[CoupleRecord]]
+}
+
+class QuillCoupleDao extends CoupleDao {
+  val logger: Logger = Logger(classOf[QuillCoupleDao])
+
+  override def storeCouple(uid: Int, pid: Int): Future[Int] = {
+    logger.info(s"Storing new couple with UID $uid and PID $pid.")
+    run(quote {
+      querySchema[CoupleRecord]("couple").insert(lift(CoupleRecord.asInsertRecord(uid, pid))).returningGenerated(_.cid)
+    })
+  }
+
+  override def getCouple(cid: Int): Future[Option[CoupleRecord]] = {
+    logger.info(s"Retrieving couple by CID $cid.")
+    run(quote {
+      querySchema[CoupleRecord]("couple").filter(_.cid == lift(cid))
+    }).map(r => r.headOption)
+  }
 }
--- src/test/scala/couple/QuillCoupleDaoTest.scala
+++ src/test/scala/couple/QuillCoupleDaoTest.scala
@@ -1,5 +1,49 @@
 package couple
 
-class QuillCoupleDaoTest {
+import db.FlywayHelper
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+import user.{QuillUserDao, UserDao, UserRegistration}
+
+import scala.util.Random
+
+class QuillCoupleDaoTest extends AsyncFlatSpec with Matchers with BeforeAndAfterEach {
+
+  val coupleDao: CoupleDao = new QuillCoupleDao
+  val userDao: UserDao = new QuillUserDao
+
+  override def beforeEach(): Unit = {
+    val flyway = FlywayHelper.getFlyway
+    flyway.clean()
+    flyway.migrate()
+  }
+
+  "storing a couple" should "return the generated CID" in {
+    (for {
+      uid <- userDao.storeUser(newUser()).map(maybeError => maybeError.right.get)
+      pid <- userDao.storeUser(newUser()).map(maybeError => maybeError.right.get)
+    } yield (uid, pid))
+      .flatMap(ids => coupleDao.storeCouple(ids._1, ids._2))
+      .map(cid => {cid should be > 0; cid})
+      .flatMap(cid => coupleDao.getCouple(cid))
+      .map(coupleRecord => {
+        coupleRecord.isDefined shouldBe true
+        coupleRecord.get.uid should be > 0
+        coupleRecord.get.pid should be > 0
+      })
+  }
+
+  "retrieving a couple that does not exist" should "return None" in {
+    coupleDao.getCouple(10).map(maybeCouple => {
+      maybeCouple.isEmpty shouldBe true
+    })
+  }
+
+  def newUser(): UserRegistration = UserRegistration.from(
+    "First", "Last", randomEmail(), "TestPassword", acceptedTerms = true, ofAge = true
+  ).right.get
+
+  def randomEmail(): String = "quillCoupleTest-" + Random.alphanumeric.take(10).mkString + "@twotest.com"
 
 }
