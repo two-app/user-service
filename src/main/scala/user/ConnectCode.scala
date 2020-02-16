6495463e70f0d7e64dfdeef4aaa5bb3885ad74ea
gerryfletch <1997Gerry@gmail.com>
2020-02-16 14:04:41 +0000

    Connect code class with HashIds support.

--- src/main/scala/user/ConnectCode.scala
+++ src/main/scala/user/ConnectCode.scala
@@ -1,5 +1,11 @@
 package user
 
-class ConnectCode {
+import com.typesafe.config.ConfigFactory
+import org.hashids.Hashids
 
+object ConnectCode {
+  def toId(connectCode: String): Option[Int] = {
+    val idParts = new Hashids(ConfigFactory.load().getString("hashids.salt"), 6).decode(connectCode)
+    if (idParts.isEmpty) None else Option(idParts(0).toInt)
+  }
 }
