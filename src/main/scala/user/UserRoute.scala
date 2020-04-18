4eb84ab7a0e509c4b0c9b915a1202b6689c89541
gerryfletch <1997Gerry@gmail.com>
2020-04-18 22:06:21 +0100

    Implemented and tested route

--- src/main/scala/user/UserRoute.scala
+++ src/main/scala/user/UserRoute.scala
@@ -2,6 +2,7 @@ package user
 
 import request.RouteDispatcher
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Route
 import cats.effect.IO
 import cats.data.EitherT
@@ -21,13 +22,20 @@ class UserRouteDispatcher(userService: UserService[IO]) extends RouteDispatcher 
   }
 
   def handleGetUser(email: String): Route = {
-    complete(email)
+    val userEffect = userRoute.getUser(email)
+    
+    onSuccess(userEffect.value.unsafeToFuture()) {
+      case Left(error: ErrorResponse) => complete(error.status, error)
+      case Right(user: User) => complete(user)
+    }
   }
 
 }
 
 class UserRoute[F[_]](userService: UserService[F]) {
 
-  def getUser(email: String): EitherT[F, ErrorResponse, User] = ???
+  def getUser(email: String): EitherT[F, ErrorResponse, User] = {
+    userService.getUser(email)
+  }
 
 }
--- src/test/scala/user/UserRouteTest.scala
+++ src/test/scala/user/UserRouteTest.scala
@@ -7,24 +7,40 @@ import db.FlywayHelper
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
 import scala.reflect.ClassTag
 import cats.effect.IO
 import config.MasterRoute
 import authentication.AuthenticationDaoStub
+import response.ErrorResponse
+import response.ErrorResponse.ClientError
+import response.ErrorResponse.NotFoundError
+import authentication.Tokens
+import authentication.AuthTestArbitraries
 
 class UserRouteTest
     extends AsyncFunSpec
     with Matchers
     with ScalatestRouteTest
-    with BeforeAndAfterEach {
+    with BeforeAndAfterEach
+    with UserTestArbitraries
+    with AuthTestArbitraries {
 
-  val route: Route = new UserRouteDispatcher(
+
+  val selfRoute: Route = new SelfRoute(
     new UserServiceImpl(
       MasterRoute.services.userDao,
       new AuthenticationDaoStub()
     )
   ).route
 
+  val route: Route = selfRoute ~ (new UserRouteDispatcher(
+    new UserServiceImpl(
+      MasterRoute.services.userDao,
+      new AuthenticationDaoStub()
+    )
+  ).route)
   override def beforeEach(): Unit = FlywayHelper.cleanMigrate()
 
   def GetUser[T: FromEntityUnmarshaller: ClassTag](email: String): T =
@@ -32,9 +48,37 @@ class UserRouteTest
       entityAs[T]
     }
 
+  def PostSelf[T: FromEntityUnmarshaller: ClassTag](
+      registration: UserRegistration
+  ): T =
+    Post("/self", registration) ~> route ~> check {
+      entityAs[T]
+    }
+
   describe("getUser") {
-    it("should return ok") {
-      GetUser[String]("test") shouldBe "test"
+    it("should return a bad request with a malformed email") {
+      GetUser[ErrorResponse]("emailATgmail.com") shouldBe ClientError(
+        "Badly formatted email."
+      )
+    }
+
+    it("should return not found for an email that doesn't exist") {
+      GetUser[ErrorResponse]("unknown@two.com") shouldBe NotFoundError(
+        "User does not exist."
+      )
+    }
+
+    it("should return a user that does exist") {
+      val registration = randomUserRegistration()
+
+      val uid = extractContext(PostSelf[Tokens](registration).accessToken).uid
+      val user = GetUser[User](registration.email)
+
+      user.uid shouldBe uid
+      user.pid shouldBe None
+      user.cid shouldBe None
+      user.firstName shouldBe registration.firstName
+      user.lastName shouldBe registration.lastName
     }
   }
 }
