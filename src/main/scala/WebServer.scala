77585cfc575255a24c3cd067af53d29740da6c52
gerryfletch <1997Gerry@gmail.com>
2020-04-23 11:53:05 +0100

    Moved to IO app

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,12 +1,17 @@
 import akka.http.scaladsl.server.{HttpApp, Route}
 import config.MasterRoute
+import cats.effect.IOApp
+import cats.effect.IO
+import cats.effect.ExitCode
 
-object Server extends HttpApp {
+class Server extends HttpApp {
   override protected def routes: Route = MasterRoute.masterRoute
 }
 
-object WebServer {
-  def main(args: Array[String]): Unit = {
-    Server.startServer("localhost", 8080)
+object WebServer extends IOApp {
+
+  def run(args: List[String]): IO[ExitCode] = {
+    new Server().startServer("localhost", 8080)
+    IO(ExitCode.Success)
   }
 }
