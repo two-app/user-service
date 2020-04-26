516b735836c40cbdf8b7fb47923f7588b11a231e
gerryfletch <1997Gerry@gmail.com>
2020-04-26 14:36:31 +0100

    More thorough logging

--- src/main/scala/health/HealthRoute.scala
+++ src/main/scala/health/HealthRoute.scala
@@ -9,9 +9,12 @@ import cats.implicits._
 import request.RouteDispatcher
 import response.ErrorResponse.InternalError
 import response.ErrorResponse
+import com.typesafe.scalalogging.Logger
 
 class HealthRouteDispatcher(healthService: HealthService[IO])
     extends RouteDispatcher {
+
+  val logger: Logger = Logger[HealthRouteDispatcher]
 
   override def route: Route = path("health") {
     get {
@@ -20,10 +23,17 @@ class HealthRouteDispatcher(healthService: HealthService[IO])
   }
 
   def handleHealthGet(): Route = {
+    logger.info("GET /health")
+    logger.info("Performing health check.")
     val healthEffect = healthService.getHealth().leftWiden[ErrorResponse]
     onSuccess(healthEffect.value.unsafeToFuture()) {
-      case Left(error: ErrorResponse) => complete(error.status, error)
-      case Right(_) => complete(StatusCodes.OK)
+      case Left(error: ErrorResponse) =>
+        logger.error("Health check failed. Responding with internal server error.")
+        complete(error.status, error)
+      case Right(_) =>
+        logger.info("Health check completed. All OK.")
+        complete(StatusCodes.OK)
+
     }
   }
 
